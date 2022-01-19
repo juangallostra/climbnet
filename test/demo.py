45d5f8f051c021a281c97b059101874ee8731726
Juan Gallostra <juangallostra@gmail.com>
2022-01-19 14:24:00 +0100

    More tests

--- test/demo.py
+++ test/demo.py
@@ -20,7 +20,46 @@ import numpy as np
 # 4. compute polygon "world" coordinates
 # 5. store all data in a json file
 
-def run_inference(image_path, model_path, output_path):
+def get_image_segments(image_file, n_v, n_w):
+    """[summary]
+
+    :param image_file: [description]
+    :type image_file: [type]
+    """
+    # open image
+    img = np.asarray(Image.open(image_file))
+    w, h, channels = img.shape
+    tiles = img.reshape(
+        n_w, w // n_w, 
+        n_v, h // n_v, 
+        channels)
+    tiles = tiles.swapaxes(1, 2).reshape(n_w * n_v, w // n_w, h // n_v, channels)
+    for idx, tile in enumerate(tiles):
+        print(tile)
+        tile_image = Image.fromarray(tile)
+        tile_image.save(f'{idx}_split.png')
+    return tiles
+
+
+def tile(filename, dir_in = "", dir_out = "", d = (None, 800) ):
+    from itertools import product
+    tile_names = []
+    name, ext = os.path.splitext(filename)
+    ext = '.png'
+    img = Image.open(os.path.join(dir_in, filename))
+    w, h = img.size
+    d_zero = d[0] if d[0] is not None else w
+    d_one = d[1] if d[1] is not None else h
+    grid = product(range(0, h-h%d_one, d_one), range(0, w-w%d_zero, d_zero))
+    for i, j in grid:
+        box = (j, i, j+d_zero, i+d_one)
+        out = os.path.join(dir_out, f'{name}_{i}_{j}{ext}')
+        tile_names.append(out)
+        img.crop(box).save(out)
+    return tile_names
+
+
+def run_inference(image_paths, model_path, output_path_prefix):
     register_coco_instances("climb_dataset", {}, "./mask.json", "")
 
     cfg = get_cfg()
@@ -40,32 +79,35 @@ def run_inference(image_path, model_path, output_path):
 
     # dataset catalog needs to exist so the polygon classes show up correctly
     DatasetCatalog.get("climb_dataset")
-    im = cv2.imread(image_path)
-    outputs = predictor(im)
+    for image_path in image_paths:
+        im = cv2.imread(image_path)
+        outputs = predictor(im)
 
-    # import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
 
-    # get hold masks
-    mask = outputs['instances'].get('pred_masks')
-    mask = mask.to('cpu')
-    _, h, w = mask.shape
-    bin_mask = np.zeros((h, w))
+        # get hold masks
+        mask = outputs['instances'].get('pred_masks')
+        mask = mask.to('cpu')
+        _, h, w = mask.shape
+        bin_mask = np.zeros((h, w))
 
-    # build binary mask
-    for m in mask:
-        bin_mask = np.add(bin_mask, np.array(m.long()))
-    cv2.imwrite(output_path, np.array(bin_mask), [cv2.IMWRITE_PNG_BILEVEL, 1])
-    cv2.imshow(output_path, np.array(bin_mask))
+        # build binary mask
+        for m in mask:
+            bin_mask = np.add(bin_mask, np.array(m.long()))
+        
+        print(output_path_prefix + image_path)
+        cv2.imwrite(output_path_prefix + image_path, np.array(bin_mask), [cv2.IMWRITE_PNG_BILEVEL, 1])
+        # cv2.imshow(output_path_prefix + image_path, np.array(bin_mask))
 
-    v = Visualizer(im[:, :, ::-1],
-                   metadata=train_metadata,
-                   #    scale=0.75,
-                   scale=0.2,
-                   instance_mode=ColorMode.IMAGE_BW  # remove the colors of unsegmented pixels
-                   )
-    v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-    cv2.imshow('climbnet', v.get_image()[:, :, ::-1])
-    cv2.waitKey(0)
+        v = Visualizer(im[:, :, ::-1],
+                    metadata=train_metadata,
+                    #    scale=0.75,
+                    scale=0.2,
+                    instance_mode=ColorMode.IMAGE_BW  # remove the colors of unsegmented pixels
+                    )
+        v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+        # cv2.imshow('climbnet', v.get_image()[:, :, ::-1])
+        # cv2.waitKey(0)
 
 def concat_images_v(images_list):
     images = [Image.open(i) for i in images_list]
@@ -98,14 +140,13 @@ def poly_approx(image):
     cv2.imwrite('contours.png', img)
     cv2.waitKey(0)
 
-def point_in_polygon(polygon_file):
+def point_in_polygon(point, polygon_file):
     from shapely.geometry import Point
     from shapely.geometry.polygon import Polygon
 
     with open(polygon_file, 'r') as f:
         polygons = json.loads(f.read()).get('holds')
 
-    point = Point(545, 150)
     Ppolygons = []
     for p in polygons:
         if len(p) >= 3:
@@ -125,7 +166,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    run_inference(args.image_path, args.model_path, args.output_image_path)
-    concat_images_v(['2.png', '3.png', '4.png']).save('all_test.png')
-    poly_approx('all_test.png')
-    print(point_in_polygon('test_pol.json'))
+    # run_inference(args.image_path, args.model_path, args.output_image_path)
+    # print(point_in_polygon('test_pol.json'))
+    # get_image_segments('test.jpg', 1, 5)
+
+    # tiles = tile('test.jpg')
+    # run_inference(tiles, args.model_path, 'out_')
+    # concat_images_v(['out_' + name for name in tiles]).save('all_test.png')
+    poly_approx('all_test_mod.png')
