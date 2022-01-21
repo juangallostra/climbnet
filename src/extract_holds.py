f08f3000b6d2cbea8879cd8e5b49aa531a40dd2a
Juan Gallostra <juangallostra@gmail.com>
2022-01-21 12:27:29 +0100

    Some more progress

--- src/extract_holds.py
+++ src/extract_holds.py
@@ -3,6 +3,8 @@ import json
 from itertools import product
 import numpy as np
 from PIL import Image
+from os import walk
+
 
 import cv2
 from detectron2 import model_zoo
@@ -117,8 +119,8 @@ def find_holds(tiles, predictor, train_metadata, device, save_tiles=True, show_r
                     instance_mode=ColorMode.IMAGE_BW  # remove the colors of unsegmented pixels
                     )
             v = v.draw_instance_predictions(outputs["instances"].to(device))
-            cv2.imshow('climbnet', v.get_image()[:, :, ::-1])
-            cv2.waitKey(0)
+            # cv2.imshow('climbnet', v.get_image()[:, :, ::-1])
+            # cv2.waitKey(0)
     return masks
 
 def get_processed_name(tile):
@@ -136,37 +138,38 @@ def concat_images_v(images_list):
         h += im.height
     return dst
 
-def main(image_path, model_path, tile_width, tile_height, combine_results):
+def main(image_paths, model_path, tile_width, tile_height, combine_results):
     if tile_width == 0:
         tile_width = None
     if tile_height == 0:
         tile_height = None
-    tiles = segment_image_into_tiles(image_path, (tile_width, tile_height), INPUT_DIR, OUTPUT_DIR)
     predictor, train_metadata, device = config_detector(model_path)
-    tile_masks = find_holds(tiles, predictor, train_metadata, device, OUTPUT_DIR)
-    dst = concat_images_v([get_processed_name(tile) for tile in tile_masks])
-    dst.save(os.path.join(OUTPUT_DIR, os.path.splitext(image_path)[0] + '_hold_masks.png'))
-    if combine_results:
-        all_mask = find_holds([INPUT_DIR + '/' + image_path], predictor, train_metadata, device, OUTPUT_DIR)
-        background = Image.open(os.path.join(OUTPUT_DIR,  os.path.splitext(image_path)[0] + '_hold_masks.png'))
-        img = Image.open(os.path.join(OUTPUT_DIR, os.path.splitext(image_path)[0] + '_processed.png'))
-        # make black pixels transparent
-        img = img.convert("RGBA")
-        datas = img.getdata()
+    for image_path in image_paths:
+        tiles = segment_image_into_tiles(image_path, (tile_width, tile_height), INPUT_DIR, OUTPUT_DIR)
+        tile_masks = find_holds(tiles, predictor, train_metadata, device, OUTPUT_DIR)
+        dst = concat_images_v([get_processed_name(tile) for tile in tile_masks])
+        dst.save(os.path.join(OUTPUT_DIR, os.path.splitext(image_path)[0] + '_hold_masks.png'))
+        if combine_results:
+            all_mask = find_holds([INPUT_DIR + '/' + image_path], predictor, train_metadata, device, OUTPUT_DIR)
+            background = Image.open(os.path.join(OUTPUT_DIR,  os.path.splitext(image_path)[0] + '_hold_masks.png'))
+            img = Image.open(os.path.join(OUTPUT_DIR, os.path.splitext(image_path)[0] + '_processed.png'))
+            # make black pixels transparent
+            img = img.convert("RGBA")
+            datas = img.getdata()
 
-        new_data = []
-        for item in datas:
-            if item[0] == 0 and item[1] == 0 and item[2] == 0:
-                new_data.append((255, 255, 255, 0))
-            else:
-                new_data.append(item)
+            new_data = []
+            for item in datas:
+                if item[0] == 0 and item[1] == 0 and item[2] == 0:
+                    new_data.append((255, 255, 255, 0))
+                else:
+                    new_data.append(item)
 
-        img.putdata(new_data)
-        background.paste(img, (0, 0), img)
-        background.save(os.path.join(OUTPUT_DIR, os.path.splitext(image_path)[0] + '_superimposed.png'),"PNG")
-        poly_approx(os.path.join(OUTPUT_DIR, os.path.splitext(image_path)[0] + '_superimposed.png'), os.path.splitext(image_path)[0])
-    else:
-        poly_approx(os.path.join(OUTPUT_DIR, os.path.splitext(image_path)[0] + '_hold_masks.png'), os.path.splitext(image_path)[0])
+            img.putdata(new_data)
+            background.paste(img, (0, 0), img)
+            background.save(os.path.join(OUTPUT_DIR, os.path.splitext(image_path)[0] + '_superimposed.png'),"PNG")
+            poly_approx(os.path.join(OUTPUT_DIR, os.path.splitext(image_path)[0] + '_superimposed.png'), os.path.splitext(image_path)[0])
+        else:
+            poly_approx(os.path.join(OUTPUT_DIR, os.path.splitext(image_path)[0] + '_hold_masks.png'), os.path.splitext(image_path)[0])
 
 def poly_approx(image, name):
     img = cv2.imread(image)
@@ -183,11 +186,11 @@ def poly_approx(image, name):
         approximations.append([a[0] for a in approx.tolist()])
         # print(approx)
         cv2.drawContours(img, [approx], 0, (0, 255, 0), 3)
-    with open('final_data/' + name + '_holds.json', 'w') as f:
+    with open('extracted_data/' + name + '_holds.json', 'w') as f:
         f.write(json.dumps(dict(holds=approximations)))
-    cv2.imshow("final", img)
+    # cv2.imshow("final", img)
     cv2.imwrite(os.path.splitext(image)[0] + '_contours.png', img)
-    cv2.waitKey(0)
+    # cv2.waitKey(0)
     return dict(holds=approximations)
 
 
@@ -196,7 +199,7 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='Hold detection')
     
-    parser.add_argument('image_file', type=str, default='', help='The input wall image file')
+    parser.add_argument('images_path', type=str, default='', help='The input wall image file')
     parser.add_argument('model_path', type=str, default='', help='climbnet model weights')
     parser.add_argument('tile_width', type=int, default=0, help='Tile width')
     parser.add_argument('tile_height', type=int, default=0, help='Tile height')
@@ -204,7 +207,13 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
 
-    main(args.image_file, args.model_path, args.tile_width, args.tile_height, args.combine_results)
+
+    f = []
+    for (dirpath, dirnames, filenames) in walk(args.images_path):
+        f.extend(filenames)
+        break
+
+    main(f, args.model_path, args.tile_width, args.tile_height, args.combine_results)
 
 # sample run:
 # > python .\extract_holds.py s1.jpg ..\model_weights\model_d2_R_50_FPN_3x.pth 0 600 False
